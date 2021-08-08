590eea974e52ea59bdb12ce8b10e8ed30cbd1f4c
IsmailHalouani-2005 <ismail.halouani@gmail.com>
2021-08-08 17:52:26 +0200

    'ninal-netflix-version-result'

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
 import MovieList from './Components/MovieList';
+import Filter from './Components/Filter';
 
 function App() {
 
@@ -19,45 +20,47 @@ function App() {
     posterURL:'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT53B_Wizqekgv5U9fetXZc_7FmMFzp5MpeEodcyTOiugrjV7iP',
     rate: 4,
   },
-
+  {
+    id:3,
+    title:'Dumbo',
+    description:'Dumbo is a 2019 American fantasy period adventure film directed by Tim Burton, with a screenplay by Ehren Kruger.',
+    posterURL:'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRgqKle6T7nhrJVaWiWoMJO03SjB76TsusDiPmisew9ZcUUdhVh',
+    rate: 4,
+  },
 ]);
+
+const [NewMovie, setNewMovie]= useState({
+  id : movie.length + 1,
+  title:'',
+  description:'',
+  posterURL:'',
+  rate:'',
+}
+);
+
+const [theBool, setTheBool] = useState(false)
 
   const formActive = () => {
     const theForm = document.querySelector('.theForm')
-    const icon = document.querySelector('.icon')
-    let bool = false
+    const icon = document.querySelector('.thePlusIcon')
 
-    if (bool == true) {
+    if (theBool== true) {
       theForm.style.transform = 'translateY(-100%)'
       theForm.style.visibility = 'hidden'
       icon.style.transform = "rotate(-45deg)"
-      bool = false
-    } else {
+      setTheBool(false)
+    } else  {
       theForm.style.transform = 'translateY(0%)'
       theForm.style.visibility = 'visible'
       icon.style.transform = "rotate(45deg)"
-      bool = true
+      setTheBool(true)
     }
 
-      console.log('is Working', icon, theForm, bool)
+      // console.log('is Working', icon, theForm, theBool)
   }
 
-  var MovieForm = document.getElementById('MovieForm')
-  MovieForm.addEventListener('submit', event => {
-    event.preventDefault()
-    const object = {
-      id:movie.lenght+1,
-      title: document.getElementById('title').value,
-      description: document.getElementById('description').value,
-      posterURL: document.getElementById('posterURL').value,
-      rate: document.getElementById('rate').value
-    }
-    if (movie.find(e => e.title == object.title && e.description == object.description && e.posterURL == object.posterURL && e.rate == object.rate)) {
-      alert('this Movie already exist !')
-    } else {
-      setMovie(movie.push(object))
-    }
-  })
+
+  const  [SearchValue, setSearchValue] = useState("");
 
   return (
     <div className="App">
@@ -67,17 +70,26 @@ function App() {
             <h1 id="Logo">YOwatch</h1>
           </div>
           <div className="addSearch">
-            <div className="search">
-              <input type="text" placeholder="Search..."/>
-              <button type="submit" className="btnInput">
-                <i class="fas fa-search"></i>
-              </button>
+            <div className="searching">
+              <div className="search">
+                <input type="search" placeholder="Search..." className="searchInput" onChange ={(e)=> setSearchValue(e.target.value)}/>
+                <button  className="btnInput">
+                  <i class="fas fa-search"></i>
+                </button>
+              </div>
+              <Filter value={SearchValue} movieList={movie}/>
             </div>
             <div className="add">
-              <i onClick={formActive} className="fas fa-plus icon"></i>
+              <i onClick={formActive} className="fas fa-plus thePlusIcon"></i>
               <div className="theForm">
-                <form className="form" id="MovieForm">
                   <input 
+                    onChange={(e)=> {
+                      console.log(e.target.value)
+                      setNewMovie(Nmovie => ({
+                        ...Nmovie,
+                        title: e.target.value,
+                      }))
+                    }}
                     className="form-control"
                     name="title" 
                     type="text" 
@@ -85,6 +97,12 @@ function App() {
                     id="title"
                   />
                   <textarea
+                    onChange={(e)=> {
+                      setNewMovie(Nmovie => ({
+                        ...Nmovie,
+                        description: e.target.value,
+                      }))
+                    }}
                     className="form-control"
                     name="description" 
                     id="" cols="30" 
@@ -93,6 +111,12 @@ function App() {
                     id="description">
                   </textarea>
                   <input
+                    onChange={(e)=> {
+                      setNewMovie(Nmovie => ({
+                        ...Nmovie,
+                        posterURL: e.target.value,
+                      }))
+                    }}
                     className="form-control"
                     name="posterURL" 
                     type="url" 
@@ -100,6 +124,12 @@ function App() {
                     id="posterURL"
                   />
                   <input
+                    onChange={(e)=> {
+                      setNewMovie(Nmovie => ({
+                        ...Nmovie,
+                        rate: e.target.value,
+                      }))
+                    }}
                     className="form-control"
                     name="rate"
                     type="number"
@@ -108,8 +138,19 @@ function App() {
                     placeholder="Rate..."
                     id="rate"
                     />
-                    <button type="submit" className="btn btn-secondary">Submit</button>
-                </form>
+                    <button onClick={async(e) => {
+                            if (movie.find(e => e.title == NewMovie.title && e.description == NewMovie.description && e.posterURL == NewMovie.posterURL)) {
+                              alert('this Movie already exist !')
+                            }else if (NewMovie.title == "" || NewMovie.description == "" || NewMovie.posterURL == "") {
+                              alert('You should enter all the neccesary data !')
+                            } else {
+                              await setMovie([...movie, NewMovie])
+                            }
+                            
+                            console.log(movie)
+
+                    }} 
+                    className="btn btn-secondary">Submit</button>
                 
               </div>
             </div>
--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -1,14 +1,44 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Filter.css';
 
-const filter = props => {
-    [title, setTitle]= useState(props.title)
-    [rate, setRate]= useState(props.rate)
-    
-    return(
-        <MovieCard title={title} rate={rate}/>
+const Filter = props => {
+
+    const theMovie = (props.value == "") ? undefined :  props.movieList.find(movie => movie.title.toLowerCase().includes(props.value.toLowerCase()) )
+    // console.log(theMovie)
+    return (
+        (theMovie) ? (
+            <div className='Fitler'>    
+                <div className="Filter-container">
+                    <div className="Filter-Image">
+                        <img src={theMovie.posterURL} className="searchImg"/>
+                    </div>.
+                    <div className="Filter-info">
+                        <h1 className="title">{theMovie.title}</h1>
+                        <p className="description">{theMovie.description}</p>
+                        <span className="stars">
+                            <strong>{theMovie.rate}</strong>
+                            <i className="fas fa-star"/>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        ) : (
+            <div className="Filter">
+                <div className="Filter-container2">
+                    No results found...
+                </div>
+            </div>
+        )
     )
+
 }
-filter.defaultProps = {
-    title: '',
-    rate: ''
+// Filter.defaultProps = {
+//     title: 'All',
+//     rate: 'All'
+// }
+Filter.propTypes = {
+    rate : PropTypes.number,
+    title: PropTypes.string,
 }
+export default Filter
